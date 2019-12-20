ebb2d7ee75e3658c4c9b0859ed7f62a20ac9afb5
Cera Smart <cera_smart@yahoo.com>
2019-12-19 21:31:37 -0600

    fixed lint errors

--- config/passport.js
+++ config/passport.js
@@ -1,165 +1,120 @@
-var bCrypt = require('bcrypt-nodejs');
+var bCrypt = require("bcrypt-nodejs");
 
 module.exports = function(passport, user) {
-    var User = user;
-    var LocalStrategy = require('passport-local').Strategy;
+  var User = user;
+  var LocalStrategy = require("passport-local").Strategy;
 
-    passport.use('local-signup', new LocalStrategy(
- 
-        {
-            usernameField: 'email',
-            passwordField: 'password',
-            passReqToCallback: true 
-     
-        },
-    
-    function(req, email, password, done) {
+  passport.use(
+    "local-signup",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true
+      },
+
+      function(req, email, password, done) {
         var generateHash = function(password) {
- 
-            return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
-         
+          return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
         };
         User.findOne({
-            where: {
-                email: email
-            }
+          where: {
+            email: email
+          }
         }).then(function(user) {
-         
-            if (user)
-         
-            {
-         
-                return done(null, false, {
-                    message: 'That email is already taken'
-                });
-         
-            } else
-         
-            {
-         
-                var userPassword = generateHash(password);
-         
-                var data =
-         
-                    {
-                        email: email,
-         
-                        password: userPassword,
-         
-                        firstname: req.body.firstname,
-         
-                        lastname: req.body.lastname
-         
-                    };
-         
-         
-                User.create(data).then(function(newUser, created) {
-         
-                    if (!newUser) {
-         
-                        return done(null, false);
-         
-                    }
-         
-                    if (newUser) {
-         
-                        return done(null, newUser);
-         
-                    }
-         
-                });
-         
-            }
-         
-        });
-    }
-    ));
-    //serialize
-    passport.serializeUser(function(user, done) {
- 
-        done(null, user.id);
- 
-    });
-    // deserialize user 
-    passport.deserializeUser(function(id, done) {
- 
-        User.findByPk(id).then(function(user) {
- 
-            if (user) {
- 
-                done(null, user.get());
- 
-            } else {
- 
-                done(user.errors, null);
- 
-            }
- 
-        });
- 
-    });
+          if (user) {
+            return done(null, false, {
+              message: "That email is already taken"
+            });
+          } else {
+            var userPassword = generateHash(password);
 
-    //LOCAL SIGNIN
-    passport.use('local-signin', new LocalStrategy(
-    {
-        usernameField: 'email',
- 
-        passwordField: 'password',
- 
-        passReqToCallback: true 
-        },
- 
- 
-    function(req, email, password, done) {
- 
+            var data = {
+              email: email,
+
+              password: userPassword,
+
+              firstname: req.body.firstname,
+
+              lastname: req.body.lastname
+            };
+
+            User.create(data).then(function(newUser, _created) {
+              if (!newUser) {
+                return done(null, false);
+              }
+
+              if (newUser) {
+                return done(null, newUser);
+              }
+            });
+          }
+        });
+      }
+    )
+  );
+  //serialize
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  // deserialize user
+  passport.deserializeUser(function(id, done) {
+    User.findByPk(id).then(function(user) {
+      if (user) {
+        done(null, user.get());
+      } else {
+        done(user.errors, null);
+      }
+    });
+  });
+
+  //LOCAL SIGNIN
+  passport.use(
+    "local-signin",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+
+        passwordField: "password",
+
+        passReqToCallback: true
+      },
+      function(req, email, password, done) {
         var User = user;
- 
+
         var isValidPassword = function(userpass, password) {
- 
-            return bCrypt.compareSync(password, userpass);
- 
-        }
- 
+          return bCrypt.compareSync(password, userpass);
+        };
+
         User.findOne({
-            where: {
-                email: email
-            }
-        }).then(function(user) {
- 
+          where: {
+            email: email
+          }
+        })
+          .then(function(user) {
             if (!user) {
- 
-                return done(null, false, {
-                    message: 'Email does not exist'
-                });
- 
+              return done(null, false, {
+                message: "Email does not exist"
+              });
             }
- 
+
             if (!isValidPassword(user.password, password)) {
- 
-                return done(null, false, {
-                    message: 'Incorrect password.'
-                });
- 
+              return done(null, false, {
+                message: "Incorrect password."
+              });
             }
- 
- 
+
             var userinfo = user.get();
             return done(null, userinfo);
- 
- 
-        }).catch(function(err) {
- 
+          })
+          .catch(function(err) {
             console.log("Error:", err);
- 
-            return done(null, false, {
-                message: 'Something went wrong with your Signin'
-            });
- 
-        });
- 
- 
-    }
- 
-));
 
-}
+            return done(null, false, {
+              message: "Something went wrong with your Signin"
+            });
+          });
+      }
+    )
+  );
+};
