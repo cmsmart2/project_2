01c81889bd3ef4d74e4ccbaa8ae79f278e7fa06f
Cera Smart <cera_smart@yahoo.com>
2020-01-02 22:01:48 -0600

    fixed background tried to add migration

--- services/cocktail.js
+++ services/cocktail.js
@@ -2,28 +2,28 @@ const axios = require("axios");
 const APIKey = process.env.COCKTAILDB_API_KEY;
 
 module.exports = {
-    search: function ({ term = null, type = null }) {
-        var queryURL =
-            "https://www.thecocktaildb.com/api/json/v2/9973533/search.php" +
-            alcoholType +
-            "?cocktail=" +
-            drinkName +
-            "&api_key=" +
-            APIKey;
+  search: function({ term = null, type = null }) {
+    var queryURL =
+      "https://www.thecocktaildb.com/api/json/v2/9973533/search.php" +
+      alcoholType +
+      "?cocktail=" +
+      drinkName +
+      "&api_key=" +
+      APIKey;
 
-        return axios.get(queryURL);
-    },
-    searchCocktail: function (cocktail) {
-        var queryURL =
-            "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=" +
-            cocktail +
-            "&api_key=" +
-            APIKey;
-        return axios.get(queryURL);
+    return axios.get(queryURL);
+  },
+  searchCocktail: function(cocktail) {
+    var queryURL =
+      "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=" +
+      cocktail +
+      "&api_key=" +
+      APIKey;
+    return axios.get(queryURL);
 
-        // console.log(cocktail, "cocktail");
-    }
-    // random: () =>
+    console.log(cocktail, "cocktail");
+  }
+  // random: () =>
 };
 
 //base template with the api key; create instance axios.
